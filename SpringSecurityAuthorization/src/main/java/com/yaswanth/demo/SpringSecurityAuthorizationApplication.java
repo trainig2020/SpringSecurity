5e04fb15f7c570573d1cdb0f3539b41045c74256
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-02 20:21:32 +0530

    Add files via upload

---
+++ SpringSecurityAuthorization/src/main/java/com/yaswanth/demo/SpringSecurityAuthorizationApplication.java
@@ -0,0 +1,13 @@
+package com.yaswanth.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityAuthorizationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityAuthorizationApplication.class, args);
+	}
+
+}
