c7a4610c79c2e3ba901bf2dc33af64fd8ce95860
trainig2020 <68064760+trainig2020@users.noreply.github.com>
2020-08-05 21:46:05 +0530

    Add files via upload
    LDAP Authentication Example

---
+++ LdapAuthentication/src/main/java/com/yaswanth/security/SecurityWebApplicationInitializer.java
@@ -0,0 +1,7 @@
+package com.yaswanth.security;
+
+import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
+
+public class SecurityWebApplicationInitializer extends AbstractSecurityWebApplicationInitializer {
+ 
+}
